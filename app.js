5b370aca3bba0e57ac251038fca9dbc2f2ee3667
mara7abumwais <1652001.com@gmail.com>
2023-12-25 19:27:33 +0200

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
-
+const cors = require('cors');
 const app = express();
 dotenv.config();
+
+app.use(cors());
 
 const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY });
 app.use(express.json());
@@ -16,15 +18,12 @@ app.post('/SkinScan-Chatbot', async (req, res) => {
   try {
     const userMessage = req.body.message;
     conversationContext.push({ role: 'user', content: userMessage });
-
     const response = await openai.chat.completions.create({
       messages: conversationContext,
       model: 'gpt-3.5-turbo',
     });
-
     const assistantReply = response.choices[0].message.content;
     res.json({ response: assistantReply });
-
     conversationContext.push({ role: 'assistant', content: assistantReply });
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
